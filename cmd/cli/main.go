06a1ded514461b29c5a35dd0cd96f1309821c63a
Amir <soltamir194@gmail.com>
2024-02-20 19:03:15 -0500

    upd

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,59 +2,43 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"sort"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 type model struct {
-	choices  []string         // items on the to-do list
-	cursor   int              // which to-do list item our cursor is pointing at
-	selected map[int]struct{} // which to-do items are selected
+	choices  []string // items on the to-do list
+	cursor   int      // which to-do list item our cursor is pointing at
+	funcs    map[string]func()
+	selected map[int]string // which to-do items are selected
 }
-
-// ==============================================================
-// Commands
-
-type templError struct{ err error }
-
-func templGenerate() tea.Cmd {
-	const line = "templ generate"
-	c := exec.Command(line)
-	return tea.ExecProcess(c, func(err error) tea.Msg {
-		return templError{err}
-	})
-}
-
-func sqlcGenerate() tea.Cmd {
-
-	// TODO: redo sqlc.yaml
-
-	const line = "sqlc generate"
-	c := exec.Command(line)
-	return tea.ExecProcess(c, func(err error) tea.Msg {
-		return templError{err}
-	})
-}
-
-// ==============================================================
 
 func initialModel() model {
 
+	choices := []string{"Templ Generate", "Sqlc Generate"}
+
+	funcs := map[string]func(){
+		choices[0]: templGenerate,
+		choices[1]: sqlcGenerate,
+		// "Push to DB",
+		// "Reset DB",
+	}
+
 	return model{
 		// Our to-do list is a grocery list
-		choices: []string{"Templ Generate", "Sqlc Generate", "Push to DB", "Reset DB"},
-
-		// A map which indicates which choices are selected. We're using
-		// the  map like a mathematical set. The keys refer to the indexes
-		// of the `choices` slice, above.
-		selected: make(map[int]struct{}),
+		choices:  choices,
+		funcs:    funcs,
+		selected: make(map[int]string),
 	}
 }
 
 func (m model) Init() tea.Cmd {
 	// Just return `nil`, which means "no I/O right now, please."
+
 	return nil
 }
 
@@ -68,7 +52,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 
 		// These keys should exit the program.
-		case "ctrl+c", "q":
+		case "ctrl+c", "q", "enter":
 			return m, tea.Quit
 
 		// The "up" and "k" keys move the cursor up
@@ -85,13 +69,15 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 		// The "enter" key and the spacebar (a literal space) toggle
 		// the selected state for the item that the cursor is pointing at.
-		case "enter", " ":
+		case " ":
 			_, ok := m.selected[m.cursor]
 			if ok {
 				delete(m.selected, m.cursor)
 			} else {
-				m.selected[m.cursor] = struct{}{}
+				m.selected[m.cursor] = m.choices[m.cursor]
 			}
+
+			// return m, m.funcs[]
 		}
 	}
 
@@ -124,16 +110,97 @@ func (m model) View() string {
 	}
 
 	// The footer
-	s += "\nPress q to quit.\n"
+	s += "\nPress Space to select, and Enter to execute.\n"
 
 	// Send the UI for rendering
 	return s
 }
 
+// ==============================================================
+// Funcs
+
+type templGenerateError struct{ err error }
+type sqlcGenerateError struct{ err error }
+
+func templGenerate() {
+	cmdName := "templ"
+	args := []string{"generate"}
+
+	cmd := exec.Command(cmdName, args...)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Printf("Output:\n%s\n", output)
+}
+
+func sqlcGenerate() {
+	updateSqlcConfig()
+
+	cmdName := "sqlc"
+	args := []string{"generate"}
+
+	cmd := exec.Command(cmdName, args...)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	fmt.Printf("Output:\n%s\n", output)
+}
+
+func updateSqlcConfig() {
+	const header = `	
+	version: "2"
+	sql: `
+
+	// entries, err := os.ReadDir("services/")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	const service = `
+	  - engine: "postgresql"
+		queries: "services/auth/authmodels/sql/query.sql"
+		schema: "services/auth/authmodels/sql/schema.sql"
+		gen:
+		  go:
+			package: "authmodels"
+			out: "services/auth/authmodels"
+			sql_package: "pgx/v5"
+	`
+
+	file, err := os.Create("test.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Write the string "hello world" to the file.
+	_, err = file.WriteString(header + service)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+// ==============================================================
+
 func main() {
-	p := tea.NewProgram(initialModel())
+	m := initialModel()
+	p := tea.NewProgram(m)
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
+	}
+
+	keys := make([]int, 0, len(m.selected))
+	for k := range m.selected {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+
+	for _, k := range keys {
+		selected := m.selected[k]
+		m.funcs[selected]()
 	}
 }
